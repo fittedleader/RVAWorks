c238f49984d75a80d6f65715bd21b41429d43ace
fittedleader <53232467+fittedleader@users.noreply.github.com>
2025-02-17 10:35:08 -0500

    update

--- client/src/sponsorPage.js
+++ client/src/sponsorPage.js
@@ -818,6 +818,9 @@ const handleSubscribe = async () => {
           </p>
           <p>P.O. Box 61025 Richmond VA</p>
         </div>
+        <div className="coded">
+          This website is coded by <a href="https://code4cause.org" target="_blank" rel="noopener noreferrer" className="code4cause-link">Code4Cause</a>
+        </div>
       </div>
 
       {/* Modal Component */}
